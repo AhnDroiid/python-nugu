7ad895bac2276d0a0daec298ecbe7585e46fde27
mrchanwoo <mschanwoo@naver.com>
2019-12-01 17:56:55 +0900

    functions implemented_2

--- crawling/riot_api.py
+++ crawling/riot_api.py
@@ -311,35 +311,35 @@ def ChamionSummary(champion_name, lane=''):
         item_recommend_list.append(tmp)
     print(item_recommend_list)
 
-
-args = {'NAME_CHAMPION': 'Ashe'}
-RecommendItemAll(**args)
-
-#print(RecommendChampionFromLane('Top'))
-#print(RecommendChampionFromChampion('Ashe'))
-# print(RecommendSkillAll(**args))
-
-player_name = "IgNar"
-
-chamion_name = 'Ashe'
-champ_summary = ChamionSummary(chamion_name)
-player_id, account_id = get_player_id(player_name)
-
-# print(player_id)
-# print(account_id)
-# r = requests.get(MATCH_HISTORY + account_id + '?api_key=' + API_KEY).json()
-# print(r)
-
-# current game!
-
-response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
-if response.status_code == 404:
-    print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
-    exit(-1)
-
-current_game_info = response.json()
-
-current_game = Game(player_name, current_game_info)
-print(current_game.players_spell)
-
+#
+# args = {'NAME_CHAMPION': 'Ashe'}
+# RecommendItemAll(**args)
+#
+# #print(RecommendChampionFromLane('Top'))
+# #print(RecommendChampionFromChampion('Ashe'))
+# # print(RecommendSkillAll(**args))
+#
+# player_name = "IgNar"
+#
+# chamion_name = 'Ashe'
+# champ_summary = ChamionSummary(chamion_name)
+# player_id, account_id = get_player_id(player_name)
+#
+# # print(player_id)
+# # print(account_id)
+# # r = requests.get(MATCH_HISTORY + account_id + '?api_key=' + API_KEY).json()
+# # print(r)
+#
+# # current game!
+#
+# response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
+# if response.status_code == 404:
+#     print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
+#     exit(-1)
+#
+# current_game_info = response.json()
+#
+# current_game = Game(player_name, current_game_info)
 # print(current_game.players_spell)
+#
+# # print(current_game.players_spell)
