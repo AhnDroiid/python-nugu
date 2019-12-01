b03c6b6638d0c5e476ecb9d4ddd313e286a87ba4
kdh0429 <kdh93429@gmail.com>
2019-12-01 22:40:18 +0900

    .

--- crawling/riot_api.py
+++ crawling/riot_api.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+#sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 from .game import Game
 from .config import Config
 from random import randint
