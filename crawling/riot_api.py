02e08a7e796deac61b1e50fa8d0b80fb7677209e
mrchanwoo <mschanwoo@naver.com>
2019-12-02 11:24:34 +0900

    c7

--- crawling/riot_api.py
+++ crawling/riot_api.py
@@ -356,25 +356,25 @@ def ChamionSummary(champion_name, lane=''):
 # #print(RecommendChampionFromLane('Top'))
 # #print(RecommendChampionFromChampion('Ashe'))
 # # print(RecommendSkillAll(**args))
-
-player_name = " Falco Lombardi"
-
-chamion_name = 'Ashe'
-champ_summary = ChamionSummary(chamion_name)
-player_id, account_id = get_player_id(player_name)
-
-
-
-# current game!
-
-response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
-if response.status_code == 404:
-    print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
-    exit(-1)
-
-current_game_info = response.json()
-
-current_game = Game(player_name, current_game_info)
+#
+# player_name = " Falco Lombardi"
+#
+# chamion_name = 'Ashe'
+# champ_summary = ChamionSummary(chamion_name)
+# player_id, account_id = get_player_id(player_name)
+#
+#
+#
+# # current game!
+#
+# response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
+# if response.status_code == 404:
+#     print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
+#     exit(-1)
+#
+# current_game_info = response.json()
+#
+# current_game = Game(player_name, current_game_info)
 # print(current_game.players_spell)
 #
 # print(current_game.players_spell)
