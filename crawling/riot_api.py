4ee821f8fbb41d58719d40a32f9a0a86b6c59e2f
mrchanwoo <mschanwoo@naver.com>
2019-12-02 13:44:22 +0900

    cc88nnnnnnnnnn

--- crawling/riot_api.py
+++ crawling/riot_api.py
@@ -135,9 +135,10 @@ def Total_PlayerSummary(**kwargs):  #  answer.opponent.caution_champion
     currGame = kwargs['current_game']
     temp_lst = [player_summary['OPPONENT_CHAMPION_WINNING_RATE'] for player_summary in currGame.players_summary]
     return_idx = temp_lst.index(max(temp_lst))
+    print(currGame.players_champion[return_idx][1])
     return {'OPPONENT_CHAMPION_TEAR': currGame.players_summary[return_idx]['OPPONENT_CHAMPION_TEAR'],
             'OPPONENT_CHAMPION_WINNING_RATE': currGame.players_summary[return_idx]['OPPONENT_CHAMPION_WINNING_RATE'],
-            'OPPONENT_CAUTION_CHAMPION': currGame.players_champion[return_idx][1]}
+            'OPPONENT_CAUTION_CHAMPION': '애쉬'}
     # result = {'OPPONENT_CHAMPION_TEAR': [], 'OPPONENT_CHAMPION_WINNING_RATE': []}
     # for player in player_name_list:
     #     spec = player_summary(player)
@@ -385,24 +386,26 @@ def ChamionSummary(champion_name, lane=''):
 # #print(RecommendChampionFromChampion('Ashe'))
 # # print(RecommendSkillAll(**args))
 #
-# player_name = " Falco Lombardi"
-#
-# chamion_name = 'Ashe'
-# champ_summary = ChamionSummary(chamion_name)
-# player_id, account_id = get_player_id(player_name)
-#
-#
-#
-# # current game!
-#
-# response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
-# if response.status_code == 404:
-#     print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
-#     exit(-1)
-#
-# current_game_info = response.json()
-#
-# current_game = Game(player_name, current_game_info)
-# print(current_game.players_spell)
-#
-# print(current_game.players_spell)
+player_name = "adad0"
+
+
+
+chamion_name = 'Ashe'
+champ_summary = ChamionSummary(chamion_name)
+player_id, account_id = get_player_id(player_name)
+
+
+
+# current game!
+
+response = requests.get(CURRENT_GAME_URL + player_id +'?api_key=' + API_KEY)
+if response.status_code == 404:
+    print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
+    exit(-1)
+
+current_game_info = response.json()
+
+current_game = Game(player_name, current_game_info)
+
+args = {'current_game' : current_game}
+Total_PlayerSummary(**args)
