a7625406fbcd1dc62b41519071357600d4f8321f
mrchanwoo <mschanwoo@naver.com>
2019-12-02 10:51:22 +0900

    c5

--- crawling/riot_api.py
+++ crawling/riot_api.py
@@ -115,8 +115,8 @@ def Total_PlayerSummary(**kwargs):  #  answer.opponent.caution_champion   ###NOT
     currGame = kwargs['current_game']
     temp_lst = [player_summary['OPPONENT_CHAMPION_WINNING_RATE'] for player_summary in currGame.players_summary]
     return_idx = temp_lst.index(max(temp_lst))
-    return {'OPPONENT_CHAMPION_TEAR': currGame.player_summary[return_idx]['OPPONENT_CHAMPION_TEAR'],
-            'OPPONENT_CHAMPION_WINNING_RATE': currGame.player_summary[return_idx]['OPPONENT_CHAMPION_WINNING_RATE'],}
+    return {'OPPONENT_CHAMPION_TEAR': currGame.players_summary[return_idx]['OPPONENT_CHAMPION_TEAR'],
+            'OPPONENT_CHAMPION_WINNING_RATE': currGame.players_summary[return_idx]['OPPONENT_CHAMPION_WINNING_RATE'],}
     # result = {'OPPONENT_CHAMPION_TEAR': [], 'OPPONENT_CHAMPION_WINNING_RATE': []}
     # for player in player_name_list:
     #     spec = player_summary(player)
