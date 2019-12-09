8b36154a72ba300326bf1343192f8daa7c6f2f65
mrchanwoo <mschanwoo@naver.com>
2019-12-09 12:01:23 +0900

    game 중아닐때추가

--- query_processor.py
+++ query_processor.py
@@ -5,18 +5,18 @@ import sys
 
 query_config =[
 	{'action_name': 'answer.opponent.specific', 'utterance' : ['NAME_OPPONENT_CHAMPION_FOR_ANALYSIS'] , 'backend': ['OPPONENT_CHAMPION_WINNING_RATE', 'OPPONENT_CHAMPION_TEAR'],
-     'function' : Specific_PlayerSummary
+     'function' : Specific_PlayerSummary, 'need' : True
      },
 	{'action_name': 'answer.opponent.caution_champion', 'utterance': [], 'backend': ['OPPONENT_CAUTION_CHAMPION', 'OPPONENT_CHAMPION_WINNING_RATE', 'OPPONENT_CHAMPION_TEAR'],
-     'function' : Total_PlayerSummary
+     'function' : Total_PlayerSummary, 'need' : True
      },
     # spell cooltime
 	{'action_name': 'answer.spell.specific_champion.specific_spell', 'utterance': ['NAME_CHAMPION_FOR_SPELL', 'NAME_SPELL'] , 'backend': ['REMAINING_TIME_OF_SPELL'],
-     'function' : AnswerSpellRemainingTime
+     'function' : AnswerSpellRemainingTime, 'need' : True
 
      },
 	{'action_name': 'answer.spell.specific_champion.wo_specific_spell', 'utterance': ['NAME_CHAMPION_FOR_SPELL', 'NAME_SPELL'], 'backend':['SPELL_NAME1', 'REMAINING_TIME1', 'SPELL_NAME2', 'REMAINING_TIME2'],
-     'function' : AnswerSpellRemainingTimeAll
+     'function' : AnswerSpellRemainingTimeAll, 'need' : True
      },
 
 	# {'action_name': 'answer.spell.all', 'utterance': ['NAME_CHAMPION_FOR_SPELL', 'NAME_SPELL'], 'backend': ['OPPONENT_CHAMPION1', 'SEPLLS_CHAMPION1', 'REMAINING_TIME_CHAMPION1'],
@@ -24,40 +24,40 @@ query_config =[
     #  },
 
      {'action_name': 'answer.friend', 'utterance': ['NAME_FRIEND'], 'backend': ['BOOL_LOG_IN'],
-     'function' : CheckFriendStatus
+     'function' : CheckFriendStatus, 'need' : False
      },
 
 	{'action_name': 'recommend_champion_lane', 'utterance': ['NAME_OPPONENT_CHAMPION', 'NAME_LANE'], 'backend': ['RECOMMENDED_CHAMPION'],
-     'function' : RecommendChampionFromLane
+     'function' : RecommendChampionFromLane, 'need' : False
      },
 
 	{'action_name': 'recommend_champion_opponent', 'utterance': ['NAME_OPPONENT_CHAMPION', 'NAME_LANE'], 'backend': ['RECOMMENDED_CHAMPION'],
-     'function' :   RecommendChampionFromChampion
+     'function' :   RecommendChampionFromChampion, 'need' : False
      },
 
 	{'action_name': 'recommend_champion_default', 'utterance': ['NAME_OPPONENT_CHAMPION', 'NAME_LANE'], 'backend': ['RECOMMENDED_CHAMPION'],
-     'function' :   RecommendRandomChampion
+     'function' :   RecommendRandomChampion, 'need' : False
      },
 
 	{'action_name': 'recommend.item.specific_core', 'utterance': ['NAME_CHAMPION_FOR_ITEM', 'NAME_NUMBER_ITEM_CORE'], 'backend': ['RECOMMENDED_ITEM_SPECIFIC'],
-     'function' : RecommendItemSpecific
+     'function' : RecommendItemSpecific, 'need' : False
      },
 
 	{'action_name': 'recommend.item.all', 'utterance':['NAME_CHAMPION_FOR_ITEM', 'NAME_NUMBER_ITEM_CORE'], 'backend': ['RECOMMENDED_ITEM_1ST', 'RECOMMENDED_ITEM_2ST',
 																																  'RECOMMENDED_ITEM_3ST'],
-    'function' : RecommendItemAll
+    'function' : RecommendItemAll, 'need' : False
      },
 
 	{'action_name': 'recommend.skill.specific', 'utterance':['NAME_CHAMPION', 'NAME_LEVEL'], 'backend': ['RECOMMENDED_SKILL_SPECIFIC'],
-     'function' : RecommendSkillSpecific
+     'function' : RecommendSkillSpecific, 'need' : False
      },
 
 	{'action_name': 'recommend.skill.all', 'utterance': ['NAME_CHAMPION', 'NAME_LEVEL'], 'backend': ['RECOMMENDED_SKILL_1ST', 'RECOMMENDED_SKILL_2ST', 'RECOMMENDED_SKILL_3ST'],
-     'function' : RecommendSkillAll
+     'function' : RecommendSkillAll, 'need' : False
      },
 
 	{'action_name': 'write.used_spell', 'utterance': ['NAME_CHAMPION_FOR_SPELL_RECORD', 'NAME_USED_SPELL'], 'backend': [],
-     'function' : RecordSpellTime
+     'function' : RecordSpellTime, 'need' : True
     }
 ]
 
@@ -67,7 +67,36 @@ def find_function_in_query(action):
             return index
     return -1
 
-def answer(query, current_game):
+
+def game_not_needed_answer(query):
+    actionName, utterance = query['action']['actionName'], query['action']['parameters']
+    idx = find_function_in_query(actionName)
+    args = {}
+    if len(utterance) != 0:
+        for keys in utterance.keys():
+            args[keys] = query['action']['parameters'][keys]['value']
+
+    if idx == -1:
+        # print("idx is -1")
+        return {'version': '2.0',
+                'resultCode': 'OK',
+                'output': {
+                }}
+
+    return_from_function = query_config[idx]['function'](**args)
+
+    result_dict = {'version': '2.0',
+                   'resultCode': 'OK',
+                   'output': {
+                   }}
+
+    for backend_parameter in query_config[idx]['backend']:
+        result_dict['output'][backend_parameter] = return_from_function[backend_parameter]
+    # print(result_dict)
+    return result_dict
+
+
+def game_needed_answer(query, current_game):
     actionName, utterance = query['action']['actionName'], query['action']['parameters']
     idx = find_function_in_query(actionName)
     args = {}
@@ -76,12 +105,8 @@ def answer(query, current_game):
         for keys in utterance.keys():
             args[keys] = query['action']['parameters'][keys]['value']
 
-    # print(idx)
-    # print(utterance)
-    # print(args)
-
     if idx == -1:
-        print("idx is -1")
+        #print("idx is -1")
         return {'version': '2.0',
 		'resultCode': 'OK',
 		'output': {
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 import json
-from query_processor import answer
+from query_processor import game_needed_answer, game_not_needed_answer, query_config
 from crawling import game, config
 from crawling.riot_api import *
 from urllib import parse
@@ -9,48 +9,35 @@ app = Flask(__name__)
 player_name = "real gankster"
 player_name_url = parse.quote(player_name)
 player_id, account_id = get_player_id(player_name_url)
-#chamion_name = 'ekko'
-#champ_summary = ChamionSummary(chamion_name)
 config_dat = Config()
 
-## current game!
-response = requests.get(CURRENT_GAME_URL + player_id + '?api_key=' + API_KEY)
-if response.status_code == 404:
-	print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
-	#exit(-1)
-
-current_game_info = response.json()
-current_game = Game(player_name, current_game_info)
-
+current_game = None
 
 @app.route('/', methods=['POST'])
 def post():
 	global current_game
-
-	#print(current_game.players_spell_used_time)
 	query = request.json
-	#response = requests.get(CURRENT_GAME_URL + player_id + '?api_key=' + API_KEY)
-	#current_game_info_query = response.json()
-	# if current_game.checkId(current_game_info_query['gameId']):
-	# 	pass
-	# else:
-	# 	current_game = Game(player_name, current_game_info_query)
-	#print(query)
 
-	return json.dumps(answer(query, current_game), ensure_ascii=False, indent=4)
-	#flag, response, current_game_new = answer(query, current_game)
+	actionName = query['action']['actionName']
 
-	# if flag == 1:
-	# 	return json.dumps(response, ensure_ascii=False, indent=4)
-	# else:
-	# 	current_game = current_game_new
-	# 	return json.dumps(response, ensure_ascii=False, indent=4)
+	for query in query_config:
+		if query['action_name'] == actionName and query['need'] == True:
+
+			## current game!
+			response = requests.get(CURRENT_GAME_URL + player_id + '?api_key=' + API_KEY)
+			if response.status_code == 404:
+				print('{}님은 현재 게임 중이 아닙니다.'.format(player_name))
+
+			if current_game is None:
+				current_game_info = response.json()
+				current_game = Game(player_name, current_game_info)
+			return json.dumps(game_needed_answer(query, current_game), ensure_ascii=False, indent=4)
+
+		elif query['action_name'] == actionName and query['need'] == False:
+			return json.dumps(game_not_needed_answer(query), ensure_ascii=False, indent=4)
+
+
 
 app.run(host='0.0.0.0', port=3389)
-
-# query = {'action': {'actionName': 'write.used_spell', 'parameters' : {'NAME_CHAMPION_FOR_SPELL_RECORD' : {'value': 'Ashe'}, 'NAME_USED_SPELL': {'value': 'Ignite'}}}}
-# flag, response, current_game_new = answer(query, current_game)
-# print(query)
-# print(response)
 
 
